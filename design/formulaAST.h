a39df09792ecc3bb5737bb41fa41e39896f7d125
Victor Ginzburg <v-viktor456@mail.ru>
2022-10-04 12:38:59 +0300

    fix

--- design/formulaAST.h
+++ design/formulaAST.h
@@ -15,20 +15,3 @@ private:
     std::unique_ptr<ASTImpl::Expr> root_expr_;
     std::forward_list<Position> cells_;
 };
-
-
-class CellExpr final : public Expr {
-public:
-    // тут я не знаю, как будет приходить, как я понял, это будет в заготовке, как строка или как структура позиции, предварительно поставил строкой
-    explicit CellExpr(std::string  cell_pos); 
-    void Print(std::ostream& out) const override;
-
-    void DoPrintFormula(std::ostream& out, ExprPrecedence /* precedence */) const override;
-
-    ExprPrecedence GetPrecedence() const override;
-
-    double Evaluate(const SheetInterface& sheet) const override;
-
-private:
-    std::string cell_pos_;
-};
